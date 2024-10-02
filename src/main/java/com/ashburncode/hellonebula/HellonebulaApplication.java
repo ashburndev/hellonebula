1c5820019d81e4f57db8ce4f0c6dba072f786e1c
ashburndev <ashburndev@gmail.com>
2024-10-02 09:31:58 -0400

    first commit

---
+++ src/main/java/com/ashburncode/hellonebula/HellonebulaApplication.java
@@ -0,0 +1,13 @@
+package com.ashburncode.hellonebula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellonebulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellonebulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashburncode/hellonebula/HellonebulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashburncode.hellonebula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellonebulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
